c17f6889c1b1e4e698c39b2ec25e9ca0d69848de
윤정민 <dev.timetravel@gmail.com>
2023-12-27 20:28:44 +0900

    컬러 시스템 추가 (#6)
    * feat: 컬러 시스템 추가

---
+++ src/styles/globalTokens.stylex.ts
@@ -0,0 +1,21 @@
+import stylex from '@stylexjs/stylex';
+
+export const color = stylex.defineVars({
+  // main color
+  darkGray: '#131313',
+
+  // sub color
+  red: '#D42727',
+
+  // grayscale
+  gray0: '#F8F8F8',
+  gray1: '#F1F1F1',
+  gray2: '#CFCFCF',
+  gray3: '#959595',
+  gray4: '#686868',
+  gray5: '#595959',
+
+  // white & black
+  white: '#FFFFFF',
+  black: '#000000',
+});
