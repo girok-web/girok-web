0d48399a6776deb82cbf05f50b64a939683cd2a6
Jungwoo <evencoding@gmail.com>
2024-01-18 22:35:03 +0900

    design: 전역 스타일을 emotion의 Global 컴포넌트를 통해 프로젝트에 적용

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
+import { Global } from '@emotion/react';
+import globalStyles from './styles/globalStyles.ts';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
+    <Global styles={globalStyles} />
     <App />
   </React.StrictMode>,
 );
