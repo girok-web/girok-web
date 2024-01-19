22b57829e728d9dbe8509786d97a184d66d0d77e
Jungwoo <evencoding@gmail.com>
2024-01-19 17:28:26 +0900

    feat: Text 컴포넌트 구현

---
+++ src/shared/Text.tsx
@@ -0,0 +1,22 @@
+import { CSSProperties } from 'react';
+import { Color, colorPalette } from '../styles/colorPalette';
+import { Typography, typographyMap } from '../styles/typography';
+import styled from '@emotion/styled';
+
+interface TextProps {
+  typography: Typography;
+  color?: Color;
+  display?: CSSProperties['display'];
+  textAlign?: CSSProperties['textAlign'];
+}
+
+const Text = styled.span<TextProps>(
+  ({ typography = 'body2' }) => typographyMap[typography],
+  ({ color = 'black', display, textAlign }) => ({
+    color: colorPalette[color],
+    display,
+    textAlign,
+  })
+);
+
+export default Text;
