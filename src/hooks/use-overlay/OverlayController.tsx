839764fd854decf2b70dba1d624ad409bdba47c8
Jungwoo <evencoding@gmail.com>
2024-01-31 22:18:20 +0900

    feat: useOverlay 구현 및 Provider 적용

---
+++ src/hooks/use-overlay/OverlayController.tsx
@@ -0,0 +1,37 @@
+import { forwardRef, Ref, useCallback, useEffect, useImperativeHandle, useState } from 'react';
+
+import { CreateOverlayElement } from './types';
+
+interface Props {
+  overlayElement: CreateOverlayElement;
+  onExit: () => void;
+}
+
+export interface OverlayControlRef {
+  close: () => void;
+}
+
+export const OverlayController = forwardRef(function OverlayController(
+  { overlayElement: OverlayElement, onExit }: Props,
+  ref: Ref<OverlayControlRef>,
+) {
+  const [isOpenOverlay, setIsOpenOverlay] = useState(false);
+
+  const handleOverlayClose = useCallback(() => setIsOpenOverlay(false), []);
+
+  useImperativeHandle(
+    ref,
+    () => {
+      return { close: handleOverlayClose };
+    },
+    [handleOverlayClose],
+  );
+
+  useEffect(() => {
+    requestAnimationFrame(() => {
+      setIsOpenOverlay(true);
+    });
+  }, []);
+
+  return <OverlayElement isOpen={isOpenOverlay} close={handleOverlayClose} exit={onExit} />;
+});
