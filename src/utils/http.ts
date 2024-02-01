a2bb1f7549ba9d914fd21ecefc55f1abf42b4627
Jungwoo <evencoding@gmail.com>
2024-02-01 19:19:13 +0900

    refactor: ApiPath 유니온 타입을 정의한다

--- src/utils/http.ts
+++ src/utils/http.ts
@@ -1,14 +1,38 @@
-import Axios from 'axios';
+import Axios, { AxiosRequestConfig } from 'axios';
 
 const axios = Axios.create({
   baseURL: import.meta.env.VITE_API_URL_DEV,
 });
 
 export const http = {
-  get: function get<Response>(url: string) {
-    return axios.get<Response>(url).then((res) => res.data);
+  get: <Response = unknown>(url: GetApiPath, config?: AxiosRequestConfig) => {
+    return axios.get<Response>(url, config).then((res) => res.data);
   },
-  post: function post<Response, Request>(url: string, body?: Request) {
-    return axios.post<Response>(url, body).then((res) => res.data);
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  post: <Request = any, Response = unknown>(url: PostApiPath, body?: Request, config?: AxiosRequestConfig) => {
+    return axios.post<Response>(url, body, config).then((res) => res.data);
+  },
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  patch: <Request = any, Response = unknown>(url: PatchApiPath, body?: Request, config?: AxiosRequestConfig) => {
+    return axios.patch<Response>(url, body, config).then((res) => res.data);
+  },
+  delete: <Response = unknown>(url: DeleteApiPath, config?: AxiosRequestConfig) => {
+    return axios.delete<Response>(url, config).then((res) => res.data);
   },
 };
+
+type GetApiPath = '/health-check' | '/auth/email/registered' | '/categories' | '/categories/id-by-path';
+
+type PostApiPath =
+  | '/sign-up'
+  | '/login'
+  | '/auth/verification-code'
+  | '/auth/verification-code/check'
+  | '/auth/password-reset/verify-code'
+  | '/auth/password-reset/code'
+  | '/categories'
+  | '/categories/path';
+
+type PatchApiPath = '/auth/reset-password' | `/categories${number}` | `/categories/${number}/parent`;
+
+type DeleteApiPath = `/categories/${number}`;
