c2ca8301ffa91cebd168a806b68e8dd598d53e3c
윤정민 <dev.timetravel@gmail.com>
2024-02-03 23:02:55 +0900

    Github Pages로 개발 버전을 배포 (#48)

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/girok-web/',
   plugins: [
     react({
       jsxImportSource: '@emotion/react',
