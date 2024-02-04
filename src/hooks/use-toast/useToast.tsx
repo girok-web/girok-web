3c747ecd8550188cd71005251c83feb4981f48f6
Jungwoo <evencoding@gmail.com>
2024-02-04 16:21:26 +0900

    refactor: 함수를 보다 역할에 맞게 분리하여 정의

--- src/hooks/use-toast/useToast.tsx
+++ src/hooks/use-toast/useToast.tsx
@@ -18,38 +18,27 @@ function useToast() {
 
   const handleClose = useCallback(() => {
     overlay.close();
-    // exit 되기 전인 0.3초 안에 다시 openToast가 호출되면 타이머에 의해 실행 될 exit 함수에 의해 즉시 언마운트된다
-    // 따라서 exit을 호출하기 전 다시 Toast를 렌더링했다면 exit을 취소해야 한다
+
     exitTimeoutRef.current = setTimeout(overlay.exit, ANIMATION_DURATION);
   }, [overlay]);
 
-  const setCloseTimeout = useCallback((closeFn: () => void, duration: number) => {
+  const initTimeout = useCallback(() => {
     if (closeTimeoutRef.current !== null) {
       clearTimeout(closeTimeoutRef.current);
     }
 
-    closeTimeoutRef.current = setTimeout(closeFn, duration);
-  }, []);
-
-  const openToast = ({ message, width = 'short' }: OpenToastOptions) => {
-    // TODO: 페어 프로그래밍으로 문제 원인 찾기: overlay.open의 콜백함수 내부에 작성하면 기대와 같이 동작하지 않는다
     if (exitTimeoutRef.current !== null) {
       clearTimeout(exitTimeoutRef.current);
     }
+  }, []);
 
-    setCloseTimeout(handleClose, TOAST_DURATION);
+  const openToast = ({ message, width = 'short' }: OpenToastOptions) => {
+    // MEMO: 이전에 설정된 타임아웃을 초기화하여 중복 실행을 방지한다
+    initTimeout();
 
-    return overlay.open(({ isOpen }) => {
-      return (
-        <Toast
-          message={message}
-          width={width}
-          isOpening={isOpen === true}
-          isClosing={isOpen === false}
-          close={handleClose}
-        />
-      );
-    });
+    closeTimeoutRef.current = setTimeout(handleClose, TOAST_DURATION);
+
+    return overlay.open(({ isOpen }) => <Toast message={message} width={width} isOpen={isOpen} close={handleClose} />);
   };
 
   return { openToast };
