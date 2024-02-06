5bc190e9cd64c78f771f97661c3c06736871a872
류정우 <88191233+jw-r@users.noreply.github.com>
2024-02-07 08:29:42 +0900

    기본 Toast 기능을 구현한다 (#36)
    * feat: useOverlay 구현 및 Provider 적용
    
    * feat: Toast 컴포넌트 구현
    
    * feat: useToast 훅 구현
    
    * design: 불필요한 css 제거
    
    * chore: use-taost로 오타가 난 폴더명을 use-toast로 변경
    
    * design: keyframes 의 코드 스타일을 일치시킨다
    
    * refactor: setTimeout 반환 타입을 ReturnType<typeof setTimeout>으로 정의하여 실행 환경에 맞는 타입을 추론하도록 수정
    
    * refactor: 실행 MODE를 식별하는 env를 vite 프로젝트에 맞게 수정
    
    * chore: Content와 모드 확인 사이에 개행 추가
    
    * design: Toast 애니메이션을 보다 자연스럽게 변경
    
    * refactor: 불필요한 코드 제거 및 기능 단위로 함수 정의
    
    * refactor: 함수를 보다 역할에 맞게 분리하여 정의
    
    * refactor: 첫 렌더링 때 null을 반환하여 fadeUp 애니메이션 실행 전 깜빡임 제거

---
+++ src/hooks/use-toast/Toast.tsx
@@ -0,0 +1,104 @@
+import { css, keyframes } from '@emotion/react';
+import styled from '@emotion/styled';
+import { colorPalette } from '../../styles/colorPalette';
+import { ANIMATION_DURATION } from './constants';
+import Text from '../../shared/Text';
+import closeIcon from '../../assets/icons/close.svg';
+import { useEffect, useState } from 'react';
+
+interface ToastProps {
+  message: string;
+  width: 'long' | 'short';
+  isOpen: boolean;
+  close: () => void;
+}
+
+function Toast({ message, width, isOpen, close }: ToastProps) {
+  const [isFirstRender, setIsFirstRender] = useState(true);
+
+  useEffect(() => {
+    setIsFirstRender(false);
+  }, []);
+
+  // MEMO: isOpen의 Default 값은 false이기 때문에 fadeUp 애니메이션 실행 전 깜빡임이 발생한다
+  // 따라서 첫 렌더링 시에는 null을 반환하여 Toast 컴포넌트를 그리지 않는다
+  return isFirstRender ? null : (
+    <ToastContainer width={width} isOpening={isOpen} isClosing={!isOpen}>
+      <Text typography="smallBody" color="white">
+        {message}
+      </Text>
+      <button onClick={close}>
+        <img src={closeIcon} alt="close" />
+      </button>
+    </ToastContainer>
+  );
+}
+
+const ToastContainer = styled.div<{ width: string; isClosing: boolean; isOpening: boolean }>`
+  position: fixed;
+  bottom: 0;
+  right: 50%;
+  transform: translate(50%, 48px);
+
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  width: ${({ width }) => (width === 'long' ? '420px' : '344px')};
+  height: 48px;
+  border-radius: 4px;
+  padding: 4px 8px 4px 16px;
+
+  background-color: ${colorPalette.gray6};
+
+  ${({ isOpening }) =>
+    isOpening &&
+    css`
+      animation: ${fadeUp} ${ANIMATION_DURATION}ms forwards;
+    `}
+  ${({ isClosing }) =>
+    isClosing &&
+    css`
+      animation: ${fadeOut} ${ANIMATION_DURATION}ms forwards;
+    `}
+`;
+
+const fadeUp = keyframes`
+  0% {
+    transform: translate(50%, 48px);
+    opacity: 0;
+  }
+  20% {
+    transform: translate(50%, -10px);
+    opacity: 1;
+  }
+  40% {
+    transform: translate(50%, 5px);
+  }
+  60% {
+    transform: translate(50%, -5px);
+  }
+  80% {
+    transform: translate(50%, 2px);
+  }
+  100% {
+    transform: translate(50%, 0);
+  }
+`;
+
+const fadeOut = keyframes`
+  0% {
+    transform: translate(50%, 0) scale(1);
+    opacity: 1;
+  }
+  50% {
+    transform: translate(50%, -10px) scale(1.05);
+    opacity: 0.5;
+  }
+  100% {
+    transform: translate(50%, 0) scale(0.95);
+    opacity: 0;
+  }
+`;
+
+export default Toast;
---
+++ src/hooks/use-toast/constants.ts
@@ -0,0 +1,2 @@
+export const TOAST_DURATION = 3000;
+export const ANIMATION_DURATION = 500;
