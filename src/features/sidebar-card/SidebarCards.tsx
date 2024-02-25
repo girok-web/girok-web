0565ca36a9d142572e55c160663988d89cba052c
Jungwoo <evencoding@gmail.com>
2024-02-26 08:11:48 +0900

    design: Sidebar Card 내부 스크롤 적용

--- src/features/sidebar-card/SidebarCards.tsx
+++ src/features/sidebar-card/SidebarCards.tsx
@@ -6,13 +6,20 @@ import TodayEventsCard from './components/TodayEventsCard';
 import TodoListCard from './components/TodoListCard';
 import CategoryCard from './components/CategoryCard';
 import Flex from '../../components/Flex';
+import { css } from '@emotion/react';
 
 function SidebarCards() {
   return (
     <Container>
       <Header />
       <Spacing size={18} />
-      <Flex direction="column" gap={24}>
+      <Flex
+        direction="column"
+        gap={24}
+        css={css`
+          padding: 0 20px 17px 20px;
+        `}
+      >
         <TodayEventsCard />
         <TodoListCard />
         <CategoryCard />
@@ -24,9 +31,8 @@ function SidebarCards() {
 const Container = styled.div`
   width: 344px;
   min-width: 344px;
-  min-height: 100vh;
-
-  padding: 17px 20px;
+  max-height: 100vh;
+  overflow-y: scroll;
 
   background-color: ${colorPalette.grayFC};
   border-radius: 0 36px 0 0;
@@ -34,6 +40,12 @@ const Container = styled.div`
   box-shadow:
     1px 1px 5px 0px #00000012,
     0px 4px 27px 0px #00000012;
+
+  // remove scroll bar style
+  -ms-overflow-style: none;
+  ::-webkit-scrollbar {
+    display: none;
+  }
 `;
 
 export default SidebarCards;
--- src/features/sidebar-card/components/Header.tsx
+++ src/features/sidebar-card/components/Header.tsx
@@ -1,14 +1,24 @@
 import Text from '../../../components/Text';
 import Icon from '../../../components/Icon';
 import Flex from '../../../components/Flex';
+import styled from '@emotion/styled';
 
 function Header() {
   return (
-    <Flex justify="space-between" align="center">
+    <Container justify="space-between" align="center">
       <Text typography="body1">girok()</Text>
       <Icon name="sidebar-left" />
-    </Flex>
+    </Container>
   );
 }
+
+const Container = styled(Flex)`
+  position: sticky;
+  top: 0;
+  background-color: inherit;
+  z-index: 50;
+
+  padding: 17px 20px 0 20px;
+`;
 
 export default Header;
