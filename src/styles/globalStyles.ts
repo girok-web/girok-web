6848bff546405273763e82f96959fbca03672431
cruelladevil <dev.timetravel@gmail.com>
2024-03-01 20:55:21 +0900

    chore: 스타일 순서 적용 및 no-empty-source 룰 삭제
    Co-authored-by: 류정우 <jw-r@users.noreply.github.com>

--- src/styles/globalStyles.ts
+++ src/styles/globalStyles.ts
@@ -2,84 +2,84 @@ import { css } from '@emotion/react';
 
 export default css`
   @font-face {
-    font-family: Pretendard;
     font-weight: 900;
-    font-display: swap;
+    font-family: Pretendard;
     src:
       local('Pretendard Black'),
       url('./assets/fonts/Pretendard-Black.woff2') format('woff2');
+    font-display: swap;
   }
 
   @font-face {
-    font-family: Pretendard;
     font-weight: 800;
-    font-display: swap;
+    font-family: Pretendard;
     src:
       local('Pretendard ExtraBold'),
       url('./assets/fonts/Pretendard-ExtraBold.woff2') format('woff2');
+    font-display: swap;
   }
 
   @font-face {
-    font-family: Pretendard;
     font-weight: 700;
-    font-display: swap;
+    font-family: Pretendard;
     src:
       local('Pretendard Bold'),
       url('./assets/fonts/Pretendard-Bold.woff2') format('woff2');
+    font-display: swap;
   }
 
   @font-face {
-    font-family: Pretendard;
     font-weight: 600;
-    font-display: swap;
+    font-family: Pretendard;
     src:
       local('Pretendard SemiBold'),
       url('./assets/fonts/Pretendard-SemiBold.woff2') format('woff2');
+    font-display: swap;
   }
 
   @font-face {
-    font-family: Pretendard;
     font-weight: 500;
-    font-display: swap;
+    font-family: Pretendard;
     src:
       local('Pretendard Medium'),
       url('./assets/fonts/Pretendard-Medium.woff2') format('woff2');
+    font-display: swap;
   }
 
   @font-face {
-    font-family: Pretendard;
     font-weight: 400;
-    font-display: swap;
+    font-family: Pretendard;
     src:
       local('Pretendard Regular'),
       url('./assets/fonts/Pretendard-Regular.woff2') format('woff2');
+    font-display: swap;
   }
 
   @font-face {
-    font-family: Pretendard;
     font-weight: 300;
-    font-display: swap;
+    font-family: Pretendard;
     src:
       local('Pretendard Light'),
       url('./assets/fonts/Pretendard-Light.woff2') format('woff2');
+    font-display: swap;
   }
 
   @font-face {
-    font-family: Pretendard;
     font-weight: 200;
-    font-display: swap;
+    font-family: Pretendard;
     src:
       local('Pretendard ExtraLight'),
       url('./assets/fonts/Pretendard-ExtraLight.woff2') format('woff2');
+    font-display: swap;
   }
 
   @font-face {
-    font-family: Pretendard;
     font-weight: 100;
-    font-display: swap;
+    font-family: Pretendard;
     src:
       local('Pretendard Thin'),
       url('./assets/fonts/Pretendard-Thin.woff2') format('woff2');
+    font-display: swap;
   }
 
   html,
@@ -165,10 +165,10 @@ export default css`
   video {
     margin: 0;
     padding: 0;
-    border: 0;
     font: inherit;
     font-size: 100%;
     vertical-align: baseline;
+    border: 0;
   }
 
   /* HTML5 display-role reset for older browsers */
@@ -228,8 +228,8 @@ export default css`
 
   a:hover,
   a:active {
-    text-decoration: none;
     color: inherit;
+    text-decoration: none;
     background-color: inherit;
   }
 
